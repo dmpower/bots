139547da7dc7f384639a54e0ddbfc02982e47151
dpower <dpower@yourwebprogrammer.com>
2015-10-30 20:52:59 -0700

    Consolidated movement and added onHitBot handler.
    The new handler required some changes in target handling throughout the
    class.

--- src/com/ywp/robocode/utils/BotTools.java
+++ src/com/ywp/robocode/utils/BotTools.java
@@ -4,19 +4,20 @@ import java.awt.geom.Point2D;
 
 import robocode.AdvancedRobot;
 import robocode.Robot;
-import robocode.ScannedRobotEvent;
 
 public class BotTools {
 
 	static public Point2D.Double project(Point2D.Double origin,double dist,double angle) {
 		return new Point2D.Double(origin.x+dist*Math.sin(angle),origin.y+dist*Math.cos(angle));
 	}
-	
-	static public Point2D.Double convertToPoint (AdvancedRobot self, ScannedRobotEvent event) {
-		return project(convertToPoint(self), event.getDistance(), event.getBearingRadians()+self.getHeadingRadians());
+
+	static public Point2D.Double convertToPoint (AdvancedRobot self, TargetBot target) {
+		return project(convertToPoint(self), target.getDistance(), target.getBearingRadians()+self.getHeadingRadians());
 	}
-	
+
 	static public Point2D.Double convertToPoint (Robot self) {
 		return new Point2D.Double(self.getX(), self.getY());
 	}
+
+
 }
---
+++ src/com/ywp/robocode/utils/TargetBot.java
@@ -0,0 +1,119 @@
+package com.ywp.robocode.utils;
+
+import robocode.HitRobotEvent;
+import robocode.ScannedRobotEvent;
+
+public class TargetBot {
+	private final String name;
+	private final double energy;
+	private final double heading;
+	private final double bearing;
+	private final double distance;
+	private final double velocity;
+	private final double time;
+
+	public TargetBot(ScannedRobotEvent event){
+		name = event.getName();
+		energy = event.getEnergy();
+		heading = event.getHeadingRadians();
+		bearing = event.getBearingRadians();
+		distance = event.getDistance();
+		velocity = event.getVelocity();
+		time = event.getTime();
+	}
+
+	public TargetBot(HitRobotEvent event){
+		name = event.getName();
+		energy = event.getEnergy();
+		heading = 0d;
+		bearing = event.getBearingRadians();
+		distance = 0d;
+		velocity = 0d;
+		time = event.getTime();
+	}
+
+	/**
+	 * Returns the bearing to the robot you scanned, relative to your robot's
+	 * heading, in degrees (-180 <= getBearing() < 180)
+	 *
+	 * @return the bearing to the robot you scanned, in degrees
+	 */
+	public double getBearing() {
+		return bearing * 180.0 / Math.PI;
+	}
+
+	/**
+	 * Returns the bearing to the robot you scanned, relative to your robot's
+	 * heading, in radians (-PI <= getBearingRadians() < PI)
+	 *
+	 * @return the bearing to the robot you scanned, in radians
+	 */
+	public double getBearingRadians() {
+		return bearing;
+	}
+
+	/**
+	 * Returns the distance to the robot (your center to his center).
+	 *
+	 * @return the distance to the robot.
+	 */
+	public double getDistance() {
+		return distance;
+	}
+
+	/**
+	 * Returns the energy of the robot.
+	 *
+	 * @return the energy of the robot
+	 */
+	public double getEnergy() {
+		return energy;
+	}
+
+	/**
+	 * Returns the heading of the robot, in degrees (0 <= getHeading() < 360)
+	 *
+	 * @return the heading of the robot, in degrees
+	 */
+	public double getHeading() {
+		return heading * 180.0 / Math.PI;
+	}
+
+	/**
+	 * Returns the heading of the robot, in radians (0 <= getHeading() < 2 * PI)
+	 *
+	 * @return the heading of the robot, in radians
+	 */
+	public double getHeadingRadians() {
+		return heading;
+	}
+
+	/**
+	 * Returns the name of the robot.
+	 *
+	 * @return the name of the robot
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the velocity of the robot.
+	 *
+	 * @return the velocity of the robot
+	 */
+	public double getVelocity() {
+		return velocity;
+	}
+
+	/**
+	 * Returns the time the target was seen.
+	 *
+	 * @return the time the target was seen
+	 */
+	public double getTime() {
+		return time;
+	}
+
+
+}
