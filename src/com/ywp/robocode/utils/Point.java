4fce81d4d1e5f0b4943cbf02f2fbf414c563afd2
Dan Power <dpower@shaklee.com>
2015-11-18 14:37:42 -0800

    added some features

--- src/com/ywp/robocode/utils/Point.java
+++ src/com/ywp/robocode/utils/Point.java
@@ -1,5 +1,7 @@
 package com.ywp.robocode.utils;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 
 import robocode.util.Utils;
@@ -22,5 +24,15 @@ public class Point extends Point2D.Double {
 
 	public double angle(Point2D.Double to) {
 		return Utils.normalRelativeAngleDegrees(Math.toDegrees(angleRadians(to)));
+	}
+
+	public void drawBot(Graphics2D g, Color c) {
+		g.setColor(c);
+		g.drawRect((int)this.x-16, (int)this.y-16, 36, 36);
+	}
+
+	public void drawPoint(Graphics2D g, Color c) {
+		g.setColor(c);
+		g.fillOval((int)this.x-2, (int)this.y-2, 4, 4);
 	}
 }
